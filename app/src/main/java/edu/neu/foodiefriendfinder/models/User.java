f3ef732de931f58aa06879957d068e21aa881cc6
zefanzhang <zhang.zef@husky.neu.edu>
2020-03-26 17:03:13 -0400

    add attributes, constructor, setter and getter

--- app/src/main/java/edu/neu/foodiefriendfinder/models/User.java
+++ app/src/main/java/edu/neu/foodiefriendfinder/models/User.java
@@ -10,12 +10,37 @@ public class User {
     public String userId;
     public String firstName;
     public String lastName;
-    public Integer phone;
+    public String phone;
     public List<String> cuisine;
     public String dob;
     public List<String> languages;
+    public String gender;
 
     public User() {
+    }
+
+    public User(String userId, String firstName, String lastName, String phone,
+                String dob, String gender) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.cuisine = cuisine;
+        this.dob = dob;
+        this.languages = languages;
+        this.gender = gender;
+    }
+
+    public User(String userId, String firstName, String lastName, String phone,
+                List<String> cuisine, String dob, List<String> languages, String gender) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.cuisine = cuisine;
+        this.dob = dob;
+        this.languages = languages;
+        this.gender = gender;
     }
 
     public String getUserId() {
@@ -66,11 +91,36 @@ public class User {
         this.languages = languages;
     }
 
-    public Integer getPhone() {
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(Integer phone) {
+    public void setPhone(String phone) {
         this.phone = phone;
     }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void addLanguage(String language) {
+        this.languages.add(language);
+    }
+
+    public void addCuisine(String cuisine) {
+        this.cuisine.add(cuisine);
+    }
+
+    public void removeLanguage(String language) {
+        this.languages.remove(language);
+    }
+
+    public void removeCuisine(String cuisine) {
+        this.cuisine.remove(cuisine);
+    }
+
 }
