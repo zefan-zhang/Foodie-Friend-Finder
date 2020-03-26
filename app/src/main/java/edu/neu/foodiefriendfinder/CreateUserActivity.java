b4a6c75952fd6a2cc83e9eed11891832322813a0
zefanzhang <zhang.zef@husky.neu.edu>
2020-03-25 21:29:05 -0400

    add create user activity, user model and register layout

---
+++ app/src/main/java/edu/neu/foodiefriendfinder/CreateUserActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.foodiefriendfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+}
