646b1b50e802f6390e0001f252b210e6fff4a447
Grant Fish <fish.gr@neu.edu>
2020-04-03 10:20:27 -0700

    got lat and lon as string to search activity

--- app/src/main/java/edu/neu/foodiefriendfinder/SearchActivity.java
+++ app/src/main/java/edu/neu/foodiefriendfinder/SearchActivity.java
@@ -2,17 +2,28 @@ package edu.neu.foodiefriendfinder;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -41,10 +52,23 @@ public class SearchActivity extends AppCompatActivity {
     private ArrayList<Integer> priceIndex = new ArrayList<>();
     private ArrayList<String> userPrice = new ArrayList<>();
 
+    private Button searchButton;
+
+    private String latitude;
+    private String longitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LatLon();
+            }
+        });
 
         cusineView = findViewById(R.id.cusineId2);
         languageView = findViewById(R.id.languageId2);
@@ -138,4 +162,22 @@ public class SearchActivity extends AppCompatActivity {
         AlertDialog dialog = dialogBuilder.create();
         return dialog;
     }
+
+
+
+    private void LatLon(){
+        LatitudeAndLongitude latitudeAndLongitude = new LatitudeAndLongitude(getApplicationContext());
+        Location location = latitudeAndLongitude.getLocation();
+
+        if(location != null){
+            double lat =  location.getLatitude();
+            latitude = Double.toString(lat);
+
+            double lon = location.getLongitude();
+            longitude = Double.toString(lon);
+
+            Toast.makeText(this, "lat: " + latitude + "\nlon: " + longitude, Toast.LENGTH_LONG ).show();
+        }
+    }
+
 }
