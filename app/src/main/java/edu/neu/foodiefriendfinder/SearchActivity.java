196091aa8a7b19f5d057a462118cb88b63bbf06c
zefanzhang <zhang.zef@husky.neu.edu>
2020-04-09 00:49:52 -0400

    adds item click in recyclerview

--- app/src/main/java/edu/neu/foodiefriendfinder/SearchActivity.java
+++ app/src/main/java/edu/neu/foodiefriendfinder/SearchActivity.java
@@ -39,50 +39,31 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class SearchActivity extends AppCompatActivity implements RestaurantsViewHolder.OnItemSelectedListener {
+public class SearchActivity extends AppCompatActivity {
 
     private Spinner cuisineDropDown;
     private Spinner distanceDropDown;
     private Spinner priceDropDown;
-    private Switch statusToggle;
-
-    private String[] priceBank;
-    private boolean[] checkedPrice;
-    private ArrayList<Integer> priceIndex = new ArrayList<>();
-    private ArrayList<String> userPrice = new ArrayList<>();
 
     private RestaurantsAdapter adapter;
-
     private static final String BASE_URL = "https://api.yelp.com/v3/";
     private static final String API_KEY = BuildConfig.API_KEY;
-    private static final String TAG = "search";
-    RecyclerView recyclerView;
 
-    private Button searchButton;
+    private static final String TAG = "searchActivity";
+
 
     private LocationListener locationListener;
     private LocationManager locationManager;
 
-    private String latitude;
-    private String longitude;
+    private double latitude;
+    private double longitude;
 
+    private ArrayList<YelpRestaurant> selectedRestaurants = new ArrayList<YelpRestaurant>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
-
-        searchButton = findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT <= 24) {
-                    LatLonOld();
-                }
-                LatLon();
-            }
-        });
 
         // cuisine select
         cuisineDropDown = findViewById(R.id.cusineSpinner);
@@ -112,56 +93,43 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
         priceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         priceDropDown.setAdapter(priceAdapter);
 
-        // status toggle
-        statusToggle = findViewById(R.id.statusToggle);
-
         Button search = findViewById(R.id.searchButton);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Used to store restaurants returned from API Call
-                ArrayList<YelpRestaurant> restaurants = new ArrayList<>();
 
                 // Yelp Params
                 String cuisine = cuisineDropDown.getSelectedItem().toString();
                 int meter = getDistance();
                 String priceRange = getPriceRange();
-                boolean userStatus = getStatus();
+                // on each click, call the function to get the current latitude and longitude.
+                LatLonOld();
 
-//                adapter = new RestaurantsAdapter(R.layout.item_restaurant, SearchActivity.this);
+                recyclerSetup();
+
+                ArrayList<YelpRestaurant> restaurants = new ArrayList<YelpRestaurant>();
 
 
-                recyclerView = findViewById(R.id.rvRestaurants);
-                recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
 
-                adapter = new RestaurantsAdapter(R.layout.item_restaurant, SearchActivity.this, restaurants, false);
-
-                recyclerView.setAdapter(adapter);
-
-                // Build the HTTP Request
                 Retrofit retrofit = new Retrofit.Builder()
                         .baseUrl(BASE_URL)
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
 
-
-                // API Call to wrap in HTTP Request -- Async callback
                 YelpService yelpService = retrofit.create(YelpService.class);
-                yelpService.searchRestaurants(API_KEY, cuisine, 42.3398,
-                        -71.0892, meter, priceRange).enqueue(new Callback<YelpDataClass>() {
+                yelpService.searchRestaurants(API_KEY,cuisine, latitude,
+                        longitude, meter, priceRange).enqueue(new Callback<YelpDataClass>() {
+
+
 
                     @Override
                     public void onResponse(Call<YelpDataClass> call, Response<YelpDataClass> response) {
                         int statusCode = response.code();
                         YelpDataClass yelpDataClass = response.body();
-
                         if (yelpDataClass == null) {
                             Log.w(TAG, "Did not receive valid response from Yelp API");
-                            Log.i(TAG, "statusCode: " + statusCode);
                             return;
                         }
-
-                        // Adds list of restaurants to the YelpRestaurants list to expose needed attributes.
                         restaurants.addAll(yelpDataClass.restaurants);
                         adapter.setRestaurants(restaurants);
 
@@ -169,7 +137,7 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
 
                     @Override
                     public void onFailure(Call<YelpDataClass> call, Throwable t) {
-                        Log.e(TAG, "Yelp API failed to return restaurant data.");
+                        // Log error here since request failed
 
                     }
                 });
@@ -178,17 +146,42 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
         });
     }
 
+    private void recyclerSetup() {
+        RecyclerView recyclerView;
+
+        adapter = new RestaurantsAdapter(R.layout.item_restaurant, SearchActivity.this);
+        recyclerView = findViewById(R.id.rvRestaurants);
+        recyclerView.setLayoutManager(new LinearLayoutManager(SearchActivity.this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new RestaurantsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Toast.makeText(SearchActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+                System.out.println(selectedRestaurants.size());
+                YelpRestaurant selectedRes = adapter.getRestanrantList().get(position);
+                if (!selectedRestaurants.contains(selectedRes)) {
+                    selectedRestaurants.add(selectedRes);
+                }
+            }
+        });
+    }
+
     public String getPriceRange() {
         String price = priceDropDown.getSelectedItem().toString();
         if (price.equals("Any Range")) {
             return "1, 2, 3, 4";
-        } else if (price.equals("$")) {
+        }
+        else if (price.equals("$")) {
             return "1";
-        } else if (price.equals("$$")) {
+        }
+        else if (price.equals("$$")) {
             return "2";
-        } else if (price.equals("$$$")) {
+        }
+        else if (price.equals("$$$")) {
             return "3";
-        } else {
+        }
+        else {
             return "4";
         }
     }
@@ -200,44 +193,23 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
         String miles = distanceDropDown.getSelectedItem().toString();
         if (miles.equals("Any distance")) {
             return 40000;
-        } else if (miles.equals("0.3 mi")) {
+        }
+        else if (miles.equals("0.3 mi")) {
             mi = 0.3;
-        } else if (miles.equals("1 mi")) {
+        }
+        else if (miles.equals("1 mi")) {
             mi = 1;
-        } else if (miles.equals("5 mi")) {
+        }
+        else if (miles.equals("5 mi")) {
             mi = 5;
-        } else if (miles.equals("20 mi")) {
+        }
+        else if (miles.equals("20 mi")) {
             mi = 20;
         }
 
         return (int) Math.round(mi * 1609.34);
 
     }
-
-    public boolean getStatus() {
-        final boolean[] currentStatus = {false};
-        statusToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            // Default value for toggle switch.
-
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // If checked, true... otherwise false.
-                currentStatus[0] = isChecked;
-            }
-        });
-        return currentStatus[0];
-    }
-
-    @Override
-    public void onItemSelected(SelectableRestaurant restaurant) {
-        List<YelpRestaurant> selectedRestaurants = adapter.getSelectedRestaurants();
-        Snackbar.make(recyclerView, "Selected restaurants are " + restaurant.getRestaurantName() +
-                ", Total selected restaurant count is " + selectedRestaurants.size(), Snackbar.LENGTH_LONG).show();
-    }
-
-    // TODO -- Implement max selection for checkboxes.
-    // TODO -- Implement sending list of selected restaurants to firebase to compare to other users who picked the same restaurants.
-
 
     private void LatLon() {
 
@@ -278,11 +250,9 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
                     locationManager.removeUpdates(locationListener);
 
 
-                    double lat = location.getLatitude();
-                    latitude = Double.toString(lat);
+                    latitude = location.getLatitude();
 
-                    double lon = location.getLongitude();
-                    longitude = Double.toString(lon);
+                    longitude = location.getLongitude();
 
 
                 }
@@ -347,15 +317,10 @@ public class SearchActivity extends AppCompatActivity implements RestaurantsView
         Location location = latitudeAndLongitude.getLocation();
 
         if (location != null) {
-            double lat = location.getLatitude();
-            latitude = Double.toString(lat);
+            latitude = location.getLatitude();
 
-            double lon = location.getLongitude();
-            longitude = Double.toString(lon);
+            longitude = location.getLongitude();
 
-            Toast.makeText(this, "lat: " + latitude + "\nlon: " + longitude, Toast.LENGTH_LONG).show();
         }
     }
 }
-
-
