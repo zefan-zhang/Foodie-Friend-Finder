45b7981cee297035ca00b5798b587fa9f5c85fa6
Alex Pho <pho.a@husky.neu.edu>
2020-04-11 01:01:24 -0400

    Added no matches checker

--- app/src/main/java/edu/neu/foodiefriendfinder/FoodieResultActivity.java
+++ app/src/main/java/edu/neu/foodiefriendfinder/FoodieResultActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import edu.neu.foodiefriendfinder.models.User;
@@ -61,11 +63,13 @@ public class FoodieResultActivity extends AppCompatActivity {
                                 }
                             }
                         }
-                        String matchfoodiesName = "";
-                        for (User user : matchFoodies) {
-                            matchfoodiesName += "  " + user.getFirstName();
-                        }
-                        textView.setText(matchfoodiesName);
+                        isNoMatches(matchFoodies);
+//                        String matchfoodiesName = "";
+//                        for (User user : matchFoodies) {
+//                            matchfoodiesName += "  " + user.getFirstName();
+//                        }
+                        isMatchExistsAlready(matchFoodies, textView);
+//                        textView.setText(matchfoodiesName);
 
                     }
 
@@ -76,5 +80,28 @@ public class FoodieResultActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    private void isNoMatches(List<User> foodieMatches) {
+        if (foodieMatches.isEmpty()) {
+            Toast.makeText(this, "Sorry no matches", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void isMatchExistsAlready(List<User> foodieMatches, TextView textView) {
+        String matchfoodiesName = "";
+        int initialSize = foodieMatches.size();
+        System.out.println("Initial size: " + initialSize);
+        for (User match : foodieMatches) {
+//            if (foodieMatches.contains(match)) {
+            if (initialSize == foodieMatches.size()) {
+                matchfoodiesName += "  " + match.getFirstName();
+            }
+            else {
+                Toast.makeText(this, "This user: " + match.getFirstName() + " is already accounted for!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        textView.setText(matchfoodiesName);
     }
 }
